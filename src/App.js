9c17c584c50a4bd18d796b7bd6a1d668de8cf718
Saidaziz <Saidaziz@Saidazizs-MBP.home>
2021-10-08 15:44:49 -0400

    installed React and Antd

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'antd/dist/antd.css'
+import { Button } from 'antd';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
+
